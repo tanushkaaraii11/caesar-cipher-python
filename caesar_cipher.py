c8f2c96c3d3b3ee05a6fa8ffce943d28bfc14454
Tanushka Rai <tanushkarai1144@gmail.com>
2026-07-12 00:21:48 +0530

    Implement Caesar Cipher encryption algorithm

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,1 +1,15 @@
 message = input("Enter the message: ")
+shift = int(input("Enter the shift value : "))
+
+encrypted_message = ""
+
+for letter in message:
+    ascii_value = ord(letter)
+    alphabet_position = ascii_value - 65
+    shifted_position = (alphabet_position + shift) % 26
+    shifted_ascii = shifted_position + 65
+    encrypted_letter = chr(shifted_ascii)
+    encrypted_message = encrypted_message + encrypted_letter
+
+    print(encrypted_message)
+    

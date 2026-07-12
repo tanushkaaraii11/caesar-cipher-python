98d5c3cb1d91828bdb4406b5d8ec568254a77965
Tanushka Rai <tanushkarai1144@gmail.com>
2026-07-12 14:19:29 +0530

    Add lowercase and special character support

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,15 +1,24 @@
 message = input("Enter the message: ")
-shift = int(input("Enter the shift value : "))
-
+shift = int(input("Enter the shift value: "))
 encrypted_message = ""
 
 for letter in message:
     ascii_value = ord(letter)
-    alphabet_position = ascii_value - 65
-    shifted_position = (alphabet_position + shift) % 26
-    shifted_ascii = shifted_position + 65
-    encrypted_letter = chr(shifted_ascii)
-    encrypted_message = encrypted_message + encrypted_letter
+    if ascii_value >= 65 and ascii_value <= 90:
+        alphabet_position = ascii_value - 65
+        shifted_position = (alphabet_position + shift) % 26
+        shifted_ascii = shifted_position + 65
+        encrypted_letter = chr(shifted_ascii)
+        encrypted_message = encrypted_message + encrypted_letter
 
-    print(encrypted_message)
-    
+    elif ascii_value >= 97 and ascii_value <= 122:
+        alphabet_position = ascii_value - 97
+        shifted_position = (alphabet_position + shift) % 26
+        shifted_ascii = shifted_position + 97
+        encrypted_letter = chr(shifted_ascii)
+        encrypted_message = encrypted_message + encrypted_letter
+
+    else:
+        encrypted_message = encrypted_message + letter
+
+print(encrypted_message)
